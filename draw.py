7e856da01329479b24d172968d49948ac6a0fb58
Busdriver26 <bus_driver@126.com>
2020-11-13 19:23:31 +0800

    draw-3

--- draw.py
+++ draw.py
@@ -2,6 +2,7 @@ from pic import imgManipulate as imp
 from rw import readWriteJson as rwj
 from calendar import TextCalendar as TC
 from timeM import timeMethods as tm
+import datetime
 
 class readJsonAndDraw:
     teRw = rwj()
@@ -15,14 +16,12 @@ class readJsonAndDraw:
                 time = dic["Time"]   #yyyy-mm-dd
                 jobs = dic["Job"]    #[t1,t2,t3,...]
                 for job in jobs:
-                    print(time+":"+str(len(time)))
                     while len(time)<10:
                         time = time+" "
                     if year:
                         output += time+"\t"+job+"\n\n"
                     else:
                         output += time[5:]+"\t"+job+"\n\n"
-            print(output)
             self.teImp.imageAddText(output,left,top,text_size=40,pathLoad = pathLoad,pathSave = pathSave)
             return pathSave
         except:
@@ -30,18 +29,18 @@ class readJsonAndDraw:
             return
 
     def drawCalendar(self,left=1000,top=500,lineHeight = 30,path="dat/dat.json",pathLoad="pic/black.png", pathSave="pic/bckgrnd.png"):
-        #try:
-        data = self.teRw.readDat()
-        output = ""
-        cal = TC()
-        temptm = tm()
-        for dic in data:
-            time = dic["Time"]
-            timeDict = temptm.getTime(time)
-        output = cal.formatmonth(timeDict["year"], timeDict["month"])
-        print(output)
-        #except:
-        #    pass
+        try:
+            temptm = tm()
+            data = self.teRw.readDat()
+            today = temptm.getTime(str(datetime.date.today()))
+            cal = TC()
+            for dic in data:
+                time = dic["Time"]
+                timeDict = temptm.getTime(time)
+            output = cal.monthdayscalendar(int(timeDict["year"]), int(timeDict["month"]))
+            print(output)
+        except:
+            pass
 
 r = readJsonAndDraw()
 r.drawCalendar()
