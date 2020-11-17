d66578e64be4e846008ac8b4c92f26e378e30f34
Busdriver26 <bus_driver@126.com>
2020-11-17 08:31:40 +0800

    "List_1"

--- draw.py
+++ draw.py
@@ -3,6 +3,7 @@ from rw import readWriteJson as rwj
 from calendar import TextCalendar as TC
 from timeM import timeMethods as tm
 import datetime
+import calendar
 
 class readJsonAndDraw:
     teRw = rwj()
@@ -11,7 +12,8 @@ class readJsonAndDraw:
     def drawTask(self,year= True,left=1000,top=500,lineHeight = 30,path="dat/dat.json",pathLoad="pic/black.png", pathSave="pic/bckgrnd.png"):
         try:
             data = self.teRw.readDat()
-            output = ""
+            outputT = ""
+            outputJ = ""
             for dic in data:
                 time = dic["Time"]   #yyyy-mm-dd
                 jobs = dic["Job"]    #[t1,t2,t3,...]
@@ -19,10 +21,12 @@ class readJsonAndDraw:
                     while len(time)<10:
                         time = time+" "
                     if year:
-                        output += time+"\t"+job+"\n\n"
+                        outputT += time+"\n"
+                        outputJ += job+"\n"
                     else:
-                        output += time[5:]+"\t"+job+"\n\n"
-            self.teImp.imageAddText(output,left,top,text_size=40,pathLoad = pathLoad,pathSave = pathSave)
+                        outputT += time[5:]+"\t"+job+"\n\n"
+            self.teImp.imageAddText(outputT,left,top,text_size=40,pathLoad = pathLoad,pathSave = pathSave)
+            self.teImp.imageAddText(outputJ,left+300,top,text_size=40,pathLoad = pathSave,pathSave = pathSave)
             return pathSave
         except:
             pass
@@ -37,10 +41,11 @@ class readJsonAndDraw:
             for dic in data:
                 time = dic["Time"]
                 timeDict = temptm.getTime(time)
-            output = cal.monthdayscalendar(int(timeDict["year"]), int(timeDict["month"]))
+            output = calendar.month(int(timeDict["year"]), int(timeDict["month"]))
             print(output)
+            self.teImp.imageAddText(output,left,top,text_size=30,pathLoad = pathLoad,pathSave = pathSave)
         except:
             pass
 
 r = readJsonAndDraw()
-r.drawCalendar()
+r.drawTask()
