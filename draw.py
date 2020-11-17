b8b834a91787100a439793cbbe89c88262389115
Busdriver26 <bus_driver@126.com>
2020-11-17 14:02:25 +0800

    draw calendar done

--- draw.py
+++ draw.py
@@ -34,7 +34,7 @@ class readJsonAndDraw:
 
     # 思路：由于排版必须列输出，每一列是某周的某一天+7重复。
     # 本版本只放本日起至下月月底的日历（beta）
-    def drawCalendar(self,left=1000,top=500,lineHeight = 30,showdates = 30,path="dat/dat.json",pathLoad="pic/black.png", pathSave="pic/bckgrnd.png"):
+    def drawCalendar(self,left=2500,top=500,lineHeight = 30,showdates = 30,path="dat/dat.json",pathLoad="pic/black.png", pathSave="pic/bckgrnd.png"):
         try:
             temptm = tm()
             data = self.teRw.readDat()
@@ -42,8 +42,9 @@ class readJsonAndDraw:
             cal = TC()
             #dates[i]的字符串就是周i+1的日期
             dates = ["Mo\n","Tu\n","We\n","Th\n","Fr\n","Sa\n","Su\n"]
+            deadLine = []
             for dat in data:
-                dates.append(temptm.getTime(dat["Time"]))
+                deadLine.append(temptm.getTime(dat["Time"]))
             thisMonth = cal.monthdays2calendar(int(today["year"]), int(today["month"]))
             if today["month"] == 12:
                 nextMonth = cal.monthdays2calendar(int(today["year"])+1, 1)
@@ -52,19 +53,26 @@ class readJsonAndDraw:
             startFlag = 0
             for week in thisMonth:
                 for days in week:
-                    if days[0]>=today["day"]:
+                    if days[0]>=int(today["day"]):
+                        if days[0] == int(today["day"]):
+                            startFlag = 1
+                            tempWeekDay = days[1]
+                        if days[0] < int(today["day"]) + 7 and days[1]<tempWeekDay:
+                            dates[days[1]] += "\n"
                         dates[days[1]] = dates[days[1]] + str(days[0]) + "\n"
-                        startFlag = 1
                     elif startFlag == 1: #当本月日期结束后退出
                         break
-                    else:
-                        dates[days[1]] += '\n'
-
-            
-            print(output)
-            self.teImp.imageAddText(output,left,top,text_size=40,pathLoad = pathLoad,pathSave = pathSave)
+            for week in nextMonth:
+                for days in week:
+                    if(days[0]!=0):
+                        dates[days[1]] = dates[days[1]] + str(days[0]) + "\n"
+            offset1 = 0
+            for col in dates:
+                self.teImp.imageAddText(col,left+offset1,top,text_size=40,pathLoad = pathSave,pathSave = pathSave,align = 'center')
+                offset1 += 100
         except:
             pass
 
 r = readJsonAndDraw()
+r.drawTask()
 r.drawCalendar()
--- pic.py
+++ pic.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 class imgManipulate:
-    def imageAddText(self,textToDraw, left, top, text_color=(255, 255, 255), text_size=70, pathLoad="pic/black.png", save=True, pathSave="pic/bckgrnd.png",ttf="ttf/Deng.ttf"):
+    def imageAddText(self,textToDraw, left, top,align = 'left' ,text_color=(255, 255, 255), text_size=70, pathLoad="pic/black.png", save=True, pathSave="pic/bckgrnd.png",ttf="ttf/Deng.ttf"):
         img = Image.open(pathLoad)
         draw = ImageDraw.Draw(img)
         fontStyle = ImageFont.truetype(ttf, text_size, encoding="utf-8")
-        draw.text((left, top), textToDraw, text_color, font=fontStyle,align = 'right')
+        draw.multiline_text((left, top), textToDraw, text_color, font=fontStyle,spacing = 20,align = align)
         if save:
             img.save(pathSave)
         return img
